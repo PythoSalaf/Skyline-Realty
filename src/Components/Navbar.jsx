7ef8bbc142a5aefdaf6654baf35404ccab962e2c
PythoSalaf <alfaheed1010@gmail.com>
2024-05-05 01:30:59 +0100

    commiting part of the home page

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -9,7 +9,7 @@ import Button from "./Button";
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   return (
-    <div className="bg-white w-full h-[70px] flex items-center">
+    <div className="bg-white w-full h-[70px] flex items-center fixed top-0">
       <div className="flex items-center  justify-between w-[96%]  md:w-[94%] lg:w-[90%] mx-auto ">
         <Link to="/" className="flex items-center gap-[2px] md:gap-1">
           <img
@@ -17,39 +17,66 @@ const Navbar = () => {
             alt="logo"
             className="w-10 h-10 md:w-11 md:h-11 lg:w-14 lg:h-14"
           />
-          <h2 className="text-blue-600 font-semibold text-[22px] md:text-[26px] lg:text-3xl">
+          <h2 className="text-[#1192ce] font-semibold text-[24px] md:text-[27px] lg:text-3xl">
             <span className="text-primary">Sky</span>Realty
           </h2>
         </Link>
-        <ul className="hidden md:flex items-center md:gap-2 lg:gap-4 ">
+        <ul className="hidden md:flex items-center md:gap-3 lg:gap-4 ">
           <li>
-            <NavLink to="/">Home</NavLink>
+            <NavLink
+              to="/"
+              className="font-semibold md:text-lg lg:text-2xl hover:text-blue-600"
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/">Listing</NavLink>
+            <NavLink
+              to="/"
+              className="font-semibold md:text-lg lg:text-2xl hover:text-blue-600"
+            >
+              Listing
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/">Agents</NavLink>
+            <NavLink
+              to="/"
+              className="font-semibold md:text-lg lg:text-2xl hover:text-blue-600"
+            >
+              Agents
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/">Properties</NavLink>
+            <NavLink
+              to="/"
+              className="font-semibold md:text-lg lg:text-2xl hover:text-blue-600"
+            >
+              Properties
+            </NavLink>
           </li>
           <li>
-            <NavLink to="/">Blog</NavLink>
+            <NavLink
+              to="/"
+              className="font-semibold md:text-lg lg:text-2xl hover:text-blue-600"
+            >
+              Blog
+            </NavLink>
           </li>
         </ul>
-        <div className="hidden md:flex items-center gap-8">
-          <div className="flex items-center gap-1">
-            <FiSearch />
-            <p>search</p>
+        <div className="hidden md:flex items-center md:gap-5 lg:gap-8">
+          <div className="flex items-center md:gap-1 lg:gap-2">
+            <FiSearch className="md:w-5 lg:w-6 md:h-5 lg:h-6" />
+            <p className="capitalize font-semibold md:text-lg lg:text-xl">
+              search
+            </p>
           </div>
           <Button
             btnName="Login"
-            btnStyle="bg-primary text-white px-6 py-[6px] rounded-lg text-lg"
+            btnStyle="bg-primary text-white px-6 py-[6px] rounded-lg md:text-lg lg:text-xl"
           />
         </div>
         <div
-          className="block md:hidden bg-primary p-[6px] text-white rounded-full cursor-pointer "
+          className="block md:hidden bg-primary p-[6px] text-white rounded-full hover:bg-black cursor-pointer "
           onClick={() => setToggle(!toggle)}
         >
           {toggle ? (
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import { Button } from "../Components";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="w-full bg-[#fff7f0] py-8 mt-[4rem] h-[80vh]">
+      <div className="w-[96%]  md:w-[94%] lg:w-[90%] mx-auto flex gap-y-5 md:gap-y-0 md:gap-x-5 flex-col md:flex-row items-start justify-between">
+        <div className="w-full ">
+          <h1 className="font-bold text-2xl md:text-3xl lg:text-4xl ">
+            Find your perfect home with us. <br /> Where you'll love to live
+          </h1>
+          <p className="text-lg font-semibold mt-6 mb-10 md:text-[22px]">
+            Our personalized approach ensures you'll love where you live. <br />{" "}
+            From luxurious estates to cozy apartments, <br /> we guide you every
+            step of the way.
+          </p>
+          <div className="bg-white h-32 rounded-xl  w-[98%] mx-auto py-2 ">
+            <div className="flex items-center justify-between w-[90%] mx-auto ">
+              <Button
+                btnName="Buy"
+                btnStyle="bg-black rounded-lg text-white hover:bg-primary px-8 py-[6px]"
+              />
+              <Button
+                btnName="Sell"
+                btnStyle="bg-black rounded-lg text-white hover:bg-primary px-8 py-[6px]"
+              />
+              <Button
+                btnName="Rent"
+                btnStyle="bg-black rounded-lg text-white hover:bg-primary px-8 py-[6px]"
+              />
+            </div>
+          </div>
+        </div>
+        <div className=" w-full"></div>
+      </div>
     </div>
   );
 };
